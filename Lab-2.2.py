71addd202c0341cefd2ccade5c15be92275360c0
AnyMoreHopeless <146988592+AnyMoreHopeless@users.noreply.github.com>
2023-11-20 12:34:50 +0300

    Update Lab-2.2.3.py

--- Lab-2.2.py
+++ Lab-2.2.py
@@ -1,15 +1,11 @@
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 money_capital = 0
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-months = 10
 for month in range(months):
-    if month == 0:
-        money = salary - spend
-    else:
-        money = salary - spend
-    money_capital -= money
-    spend = spend * (1 + increase)
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital))
+    money_capital += salary - spend
+    spend += spend * increase
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {-money_capital:0.2f}")
