dafaf72a918fed90adaab1c18ee332a5fc7922f3
AnyMoreHopeless <146988592+AnyMoreHopeless@users.noreply.github.com>
2023-11-20 12:24:43 +0300

    Update Lab-2.1.1.py

--- Lab-2.1.py
+++ Lab-2.1.py
@@ -2,17 +2,11 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-new_spend = spend * (1 + increase)
 months = 0
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 while money_capital >= 0:
-    if months == 0:
-        money = salary - spend
-    else:
-        money = salary - new_spend
-    new_spend *= 1 + increase
+    money_capital += salary
+    money_capital -= spend
+    spend *= (1 + increase)
     months += 1
-    money_capital += money
-
-print("Количество месяцев, которое можно протянуть без долгов:", months-1)
+print("Можно прожить без долгов", months, "месяцев")
